41813702d8a6cc6443d3a5b5aa0269874417397c
rozhik <rozhik@protonmail.com>
2026-03-04 19:08:24 +0200

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,25 @@
 import {NavLink} from 'react-router-dom'
 import {ShoppingCart, User} from 'lucide-react';
+import logo from '../images/logo.svg';
 
 
 function Header() {
     return (<div className="max-w-7xl mx-auto w-full py-4 px-2">
         <div className="flex justify-between items-center border-b">
-            <div>
-                <a href="#" className="text-2xl font-bold">Online-Store</a>
+            <div className='flex items-center justify-center gap-10'>
+                <div>
+                    <a href="#"><img src={logo} className="max-w-15" alt=""/></a>
+                </div>
+                <div className="flex gap-3 m-2 py-3 px-3 bg-white text-black font-medium text-lg">
+                    <NavLink to="/"
+                             className={({isActive}) => "pr-3 " + (isActive ? "text-black border-black pb-1" : "text-gray-600 hover:text-black transition-colors")}> Main</NavLink>
+                    <NavLink to="/catalog"
+                             className={({isActive}) => "pr-3 " + (isActive ? "text-black border-black pb-1" : "text-gray-600 hover:text-black transition-colors")}> Catalog</NavLink>
+                    <a href="#" className="pr-3 text-gray-600 hover:text-black transition-colors">Support</a>
+                    <a href="#" className="pr-3 text-gray-600 hover:text-black transition-colors">About Us</a>
+                </div>
             </div>
-            <div className="flex gap-5 m-2 py-3 px-3 bg-white text-black font-bold text-lg divide-x">
-                <NavLink to="/"
-                         className={({isActive}) => "pr-3 " + (isActive ? "text-black border-black pb-1" : "text-gray-600 hover:text-black transition-colors")}> Main</NavLink>
-                <NavLink to="/catalog"
-                         className={({isActive}) => "pr-3 " + (isActive ? "text-black border-black pb-1" : "text-gray-600 hover:text-black transition-colors")}> Catalog</NavLink>
-                <a href="#" className="pr-3 text-gray-600 hover:text-black transition-colors">Support</a>
-                <a href="#" className="pr-3 text-gray-600 hover:text-black transition-colors">About Us</a>
-            </div>
+
             <div className='flex gap-5'>
                 <a href="#"><ShoppingCart/></a>
                 <a href="#"> <User/></a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import re_banner from '../images/resident-evil-requiem-banner.jpg';
 
 function Hero() {
     return (<div className='max-w-7xl mx-auto w-full py-4 px-2 relative'>
-        <a href=""><img src={re_banner} alt="" className=" rounded-4xl"/>
+        <a href=""><img src={re_banner} alt="" className="rounded-4xl max-w-0"/>
             <div className='absolute bottom-56 left-60'>
                 <p className=' rounded-2xl py-2 px-4 bg-red-950 font-bold text-2xl'>Preorder</p>
             </div>
